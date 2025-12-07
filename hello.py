ba281025a073712c5640b2c6bd5d3072eb29efa9
Ali Awan <aliawan8240@gmail.com>
2025-12-07 11:15:39 +0500

    Master Branch Created

---
+++ hello.py
@@ -0,0 +1,21 @@
+# n1 = input("enter first nunmber! ")
+# n2 = input("\n enter second number! ")
+# sum = int(n1) + int(n2)
+
+# print("\n")
+# print("the sum of number is: " , sum)
+
+# for i in range(0,10 ,10) :
+#     print("hello: ", i)
+
+# i=1
+# while(i<=10) :
+#     print("hello: ",i)
+#     i = i+1
+import datetime
+
+print(datetime.datetime.now())
+names = ["ali", "hassan", "ahmad", "ahsan"]
+
+for  i in names:
+    print("Name: ", i)
